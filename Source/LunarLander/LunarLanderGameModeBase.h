277c8c56f5ad05bc101cf25110c7c37d65e43776
Alejandro Ulloa <alejandroekas@gmail.com>
2023-04-17 01:08:55 -0400

    project created

---
+++ Source/LunarLander/LunarLanderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LunarLanderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNARLANDER_API ALunarLanderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
